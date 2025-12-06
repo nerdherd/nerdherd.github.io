e9fd9a34477d5c89f4b9b351d1487f25adfc2db3
Magic Man <65839659+Magician357@users.noreply.github.com>
2025-12-06 11:41:01 -0800

    basic html

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var last_scroll_top = 0;
+
+const header = document.getElementById("header");
+
+onscroll = (event) => {
+    var scroll_top = document.documentElement.scrollTop;
+    if (scroll_top < 100 || scroll_top < last_scroll_top) {
+        header.classList.remove("hide");
+    } 
+    else if (scroll_top > last_scroll_top) {
+        header.classList.add("hide");
+    } 
+    last_scroll_top = scroll_top <= 0 ? 0 : scroll_top;
+}
