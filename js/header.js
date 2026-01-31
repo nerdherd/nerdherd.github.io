a2290bf0f725e6868d578e037e1d251676611b4a
Magic Man <65839659+Magician357@users.noreply.github.com>
2026-01-30 16:47:49 -0800

    make header better on mobiles

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("nav");
+const spacer = document.getElementById("spacer");
+function toggleNav(){
+    nav.classList.toggle("show");
+    // spacer.classList.toggle("show");
+}
