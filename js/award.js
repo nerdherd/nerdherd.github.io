fafed40311d8a29fca9cee6a0cdb7f1202c15795
John-Cams <143854397+John-Cams@users.noreply.github.com>
2026-03-14 12:41:31 -0700

    Added new tab, video, and docs

---
+++ js/award.js
@@ -0,0 +1,34 @@
+var currentAward = 0;
+
+const award1list1 = document.getElementById("award1-list-1");
+const award1list2 = document.getElementById("award1-list-2");
+const award1header = document.getElementById("award1-header")
+const award1List = [award1, award2]
+
+function switchUp(){
+    currentAward += 1;
+    if(currentAward >= awardList.length){
+        currentAward = 0;
+    }
+    for (let item of awardList){
+        item.style.display = "none";
+    }
+    awardList[currentAward].style.display = "flex";
+}
+
+function switchDown(){
+    currentAward -= 1;
+    if(currentAward < 0){
+        currentAward = awardList.length-1;
+    }
+    for (let item of awardList){
+        item.style.display = "none";
+    }
+    awardList[currentAward].style.display = "flex";
+}
+
+function listOfId(){
+    
+}
+
+
