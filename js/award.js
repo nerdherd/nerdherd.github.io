2949bc33318e93768744bee596be32aecf1d371d
John-Cams <143854397+John-Cams@users.noreply.github.com>
2026-03-17 11:24:24 -0700

    Worked on an award display interface

--- js/award.js
+++ js/award.js
@@ -1,34 +1,36 @@
-var currentAward = 0;
+// var currentAward = 0;
 
-const award1list1 = document.getElementById("award1-list-1");
-const award1list2 = document.getElementById("award1-list-2");
-const award1header = document.getElementById("award1-header")
-const award1List = [award1, award2]
+// const award1list1 = document.getElementById("award1-list-1");
+// const award1list2 = document.getElementById("award1-list-2");
+// const award1header = document.getElementById("award1-header")
+// const award1List = [award1, award2]
 
-function switchUp(){
-    currentAward += 1;
-    if(currentAward >= awardList.length){
-        currentAward = 0;
-    }
-    for (let item of awardList){
-        item.style.display = "none";
-    }
-    awardList[currentAward].style.display = "flex";
-}
+// function switchUp(){
+//     currentAward += 1;
+//     if(currentAward >= awardList.length){
+//         currentAward = 0;
+//     }
+//     for (let item of awardList){
+//         item.style.display = "none";
+//     }
+//     awardList[currentAward].style.display = "flex";
+// }
 
-function switchDown(){
-    currentAward -= 1;
-    if(currentAward < 0){
-        currentAward = awardList.length-1;
-    }
-    for (let item of awardList){
-        item.style.display = "none";
-    }
-    awardList[currentAward].style.display = "flex";
-}
+// function switchDown(){
+//     currentAward -= 1;
+//     if(currentAward < 0){
+//         currentAward = awardList.length-1;
+//     }
+//     for (let item of awardList){
+//         item.style.display = "none";
+//     }
+//     awardList[currentAward].style.display = "flex";
+// }
 
-function listOfId(){
-    
-}
+// function test(a, b){
+//     for (let item of b){
+//         alert(item);
+//     }
+// }
 
 
