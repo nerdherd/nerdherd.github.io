b718056d20cdc4db56720e1e2d862733d780ae9e
John-Cams <143854397+John-Cams@users.noreply.github.com>
2026-03-28 12:30:48 -0700

    Added awards

--- js/award.js
+++ js/award.js
@@ -1,36 +1,31 @@
-// var currentAward = 0;
+var currentAward = 0;
 
-// const award1list1 = document.getElementById("award1-list-1");
-// const award1list2 = document.getElementById("award1-list-2");
-// const award1header = document.getElementById("award1-header")
-// const award1List = [award1, award2]
+function switchUp(a, b){
+    currentAward += 1;
+    if(currentAward >= document.getElementById('awardList').length){
+        currentAward = 0;
+    }
+    for (let item of document.getElementById('awardList')){
+        item.style.display = "none";
+    }
+    document.getElementById('awardList')[currentAward].style.display = "flex";
+}
 
-// function switchUp(){
-//     currentAward += 1;
-//     if(currentAward >= awardList.length){
-//         currentAward = 0;
-//     }
-//     for (let item of awardList){
-//         item.style.display = "none";
-//     }
-//     awardList[currentAward].style.display = "flex";
-// }
+function switchDown(){
+    currentAward -= 1;
+    if(currentAward < 0){
+        currentAward = awardList.length-1;
+    }
+    for (let item of awardList){
+        item.style.display = "none";
+    }
+    awardList[currentAward].style.display = "flex";
+}
 
-// function switchDown(){
-//     currentAward -= 1;
-//     if(currentAward < 0){
-//         currentAward = awardList.length-1;
-//     }
-//     for (let item of awardList){
-//         item.style.display = "none";
-//     }
-//     awardList[currentAward].style.display = "flex";
-// }
-
-// function test(a, b){
-//     for (let item of b){
-//         alert(item);
-//     }
-// }
+function test(a, b){
+    for (let item of b){
+        alert(item);
+    }
+}
 
 
