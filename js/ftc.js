b812e0f50836df1456b384947669bcc91e984426
John-Cams <143854397+John-Cams@users.noreply.github.com>
2026-09-05 10:51:07 -0700

    Updated ftc

--- js/ftc.js
+++ js/ftc.js
@@ -1,14 +1,27 @@
 var vars = document.querySelector(':root').style;
+var isGreen = true;
 
 document.addEventListener('DOMContentLoaded', function() {
     green();
 });
+
+document.getElementById("ftc-switch").addEventListener('click', (event) => {
+    if(isGreen){
+        purple();
+    } else {
+        green();
+    }
+    isGreen = !isGreen
+})
 
 function green(){
     vars.setProperty('--nerd-herd-blue', '#3E7D51');
     vars.setProperty('--nerd-herd-navy', '#1f5b38');
     document.getElementById("seven-squared").style.display = "flex";
     document.getElementById("singularity").style.display = "none";
+    document.getElementById("logo-bot").src = "../images/ftc/ftc49.jpg";
+    document.getElementById("logo-top").src = "../images/ftc/ftc49.jpg";
+    document.getElementById("ftc-switch-image").src = "../images/ftc/49_switch.png";
 }
 
 function purple(){
@@ -16,4 +29,7 @@ function purple(){
     vars.setProperty('--nerd-herd-navy', '#533e6d');
     document.getElementById("seven-squared").style.display = "none";
     document.getElementById("singularity").style.display = "flex";
+    document.getElementById("logo-bot").src = "../images/ftc/ftc51.jpg";
+    document.getElementById("logo-top").src = "../images/ftc/ftc51.jpg";
+    document.getElementById("ftc-switch-image").src = "../images/ftc/51_switch.png";
 }
